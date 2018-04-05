14fc264e0d5f6caf32ac1047e287866115677f48
Zoilo Dela Cruz <zoilocruz@freedom.com>
2018-04-05 17:40:32 +0800

    update component

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,23 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <div>
+      <h1>{{ title }}</h1>
+    </div>
+    <ninjas></ninjas>
   </div>
 </template>
 
 <script>
+import Ninjas from './components/Ninjas.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {'ninjas': Ninjas},
+  data: function () {
+    return {
+      title: 'Your first view file, wooooo!'
+    }
+  }
 }
 </script>
 
@@ -20,4 +31,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+  h1 {
+    color: purple;
+  }
 </style>
---
+++ src/components/Ninjas.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>List of Ninjas</h1>
+    <ul>
+      <li v-for="(ninja, index) in ninjas" :key="index">{{ ninja }}</li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'ninjas',
+  data: function () {
+    return {
+      ninjas: ['Yoshi', 'Mario', 'Ryu']
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1{
+  color: green;
+}
+</style>
